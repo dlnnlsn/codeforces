e6c070ae43f09ed8a5dd42cb70de022a6b9ce1e2
Dylan Nelson <nlsdyl001@myuct.ac.za>
2022-03-27 02:06:03 +0200

    add sparse table based solution for 359D - Pair of Numbers

--- archive/359d_pair_of_numbers.cpp
+++ archive/359d_pair_of_numbers.cpp
@@ -1,37 +1,39 @@
-#include <functional>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-void build_tree(vector<int>& tree, const vector<int>& values, int node, int left, int right, function<int(int, int)> op) {
-    if (left == right) {
-        tree[node] = values[left];
-        return;
+vector<int> logs;
+
+void initialize_logs(int n) {
+    logs.push_back(-1);
+    int next_power_of_two = 1;
+    int log = -1;
+    for (int i = 1; i <= n; ++i) {
+        if (i == next_power_of_two) {
+            next_power_of_two *= 2;
+            ++log;
+        }
+        logs.push_back(log);
     }
-    int mid = (left + right) / 2;
-    build_tree(tree, values, node * 2, left, mid, op);
-    build_tree(tree, values, node * 2 + 1, mid + 1, right, op);
-    tree[node] = op(tree[2 * node], tree[2 * node + 1]); 
 }
 
-vector<int> build_tree(const vector<int>& values, function<int(int, int)> op) {
-    vector<int> tree(4 * values.size() + 1, 0);
-    build_tree(tree, values, 1, 0, values.size() - 1, op);
-    return tree; 
+vector<vector<int>> build_table(const vector<int>& nums, int (*op)(int, int)) {
+    vector<vector<int>> table;
+    table.push_back(vector<int>(nums));
+    for (int power_of_two = 1; power_of_two < nums.size(); power_of_two *= 2) {
+        vector<int> row = vector<int>(table.back());
+        for (int i = 0; i < nums.size() - power_of_two; ++i) {
+            row[i] = op(row[i], row[i + power_of_two]);
+        }
+        table.push_back(row);
+    }
+    return table;
 }
 
-int query(const vector<int>& tree, int node, int tree_left, int tree_right, int left, int right, function<int(int, int)> op) {
-    if ((left == tree_left) && (right == tree_right)) return tree[node];
-    int tree_mid = (tree_left + tree_right) / 2;
-    if (left > tree_mid) return query(tree, 2 * node + 1, tree_mid + 1, tree_right, left, right, op);
-    if (tree_mid + 1 > right) return query(tree, 2 * node, tree_left, tree_mid, left, right, op);
-    return op(query(tree, 2 * node, tree_left, tree_mid, left, tree_mid, op),
-              query(tree, 2 * node + 1, tree_mid + 1, tree_right, tree_mid + 1, right, op));
-}
-
-int query(const vector<int>& tree, int nums, int left, int right, function<int(int, int)> op) {
-    return query(tree, 1, 0, nums - 1, left, right, op);
+int query(const vector<vector<int>>& table, int left, int right, int (*op)(int, int)) {
+    const int row = logs[right - left + 1];
+    return op(table[row][left], table[row][right - (1 << row) + 1]);
 }
 
 int gcd(int a, int b) {
@@ -49,20 +51,22 @@ int my_min(int a, int b) {
 
 int main() {
     int n; cin >> n;
-    vector<int> nums; nums.reserve(n);
-    for (int i = 0; i < n; ++i) {
+    initialize_logs(n);
+
+    vector<int> nums;
+    for (int i = 0; i < n; i++) {
         int num; cin >> num; nums.push_back(num);
     }
 
-    vector<int> gcd_tree = build_tree(nums, gcd);
-    vector<int> min_tree = build_tree(nums, my_min);
+    vector<vector<int>> gcd_table = build_table(nums, gcd);
+    vector<vector<int>> min_table = build_table(nums, my_min); 
 
     int lo = 0; int hi = n;
     while ((hi - lo) > 1) {
         int mid = (hi + lo) / 2;
         bool works = false;
-        for (int i = 0; i + mid < n; ++i) {
-            if (query(gcd_tree, n, i, i + mid, gcd) == query(min_tree, n, i, i + mid, my_min)) {
+        for (int i = 0; i < n - mid; ++i) {
+            if (query(gcd_table, i, i + mid, gcd) == query(min_table, i, i + mid, my_min)) {
                 works = true;
                 break;
             }
@@ -72,8 +76,8 @@ int main() {
     }
 
     vector<int> pairs;
-    for (int i = 0; i + lo < n; ++i) {
-        if (query(gcd_tree, n, i, i + lo, gcd) == query(min_tree, n, i, i + lo, my_min)) pairs.push_back(i + 1);
+    for (int i = 0; i < n - lo; ++i) {
+        if (query(gcd_table, i, i + lo, gcd) == query(min_table, i, i + lo, my_min)) pairs.push_back(i + 1);
     }
 
     cout << pairs.size() << " " << lo << endl;
